3be23c82fdef64ba75dd770716ff56852784209c
Martin Dimitrov <martin@sachmata.com>
2022-09-02 19:12:40 +0300

    Add wind compass

---
+++ components/wind-compass.js
@@ -0,0 +1,42 @@
+import clsx from 'clsx'
+import { useMemo } from 'react'
+
+const WIND_DIRECTION_CODE = [
+    'n',
+    'nne',
+    'ne',
+    'ene',
+    'e',
+    'ese',
+    'se',
+    'sse',
+    's',
+    'ssw',
+    'sw',
+    'wsw',
+    'w',
+    'wnw',
+    'nw',
+    'nnw',
+]
+
+const WIND_DIRECTION_SECTOR = 360 / WIND_DIRECTION_CODE.length
+
+export default function WindCompass({ directionDegrees }) {
+    const windCode = useMemo(() => {
+        const direction = ((directionDegrees ?? 0) + WIND_DIRECTION_SECTOR / 2) % 360
+        const index = Math.round(direction / WIND_DIRECTION_SECTOR)
+        const code = WIND_DIRECTION_CODE[index]
+
+        return code
+    }, [directionDegrees])
+
+    return (
+        <div className="compass">
+            <div className="direction">
+                <p>{windCode?.toUpperCase() ?? ''}</p>
+            </div>
+            <div className={clsx('arrow', windCode)}></div>
+        </div>
+    )
+}
--- pages/station/[id].js
+++ pages/station/[id].js
@@ -1,69 +1,79 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react'
 
-import { useRouter } from "next/router";
-import Head from "next/head";
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 
-import styles from "../../styles/Station.module.css";
+import styles from '../../styles/Station.module.css'
 
-import usePoll from "../../components/use-poll";
-import kitingApi from "../../kiting-api";
+import usePoll from '../../components/use-poll'
+import kitingApi from '../../kiting-api'
+import WindCompass from '../../components/wind-compass'
 
 export default function Station({ station }) {
-    const router = useRouter();
+    const router = useRouter()
 
-    const { id } = router.query;
+    const { id } = router.query
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState(null)
 
     const pollCb = useCallback(async () => {
-        const apiRes = await fetch(`/api/kiting-live/observations/latest/${id}`);
+        const apiRes = await fetch(`/api/kiting-live/observations/latest/${id}`)
 
         if (!apiRes.ok) {
-            setData(null);
+            setData(null)
         } else {
-            const apiData = await apiRes.json();
-            setData(apiData);
+            const apiData = await apiRes.json()
+            setData(apiData)
         }
-    }, [id]);
+    }, [id])
 
-    usePoll(pollCb, 5e3);
+    usePoll(pollCb, 5e3)
 
     return (
         <div className={styles.container}>
             <Head>
-                <title>{`kiting.live dash - ${id ?? ""}`}</title>
+                <title>{`kiting.live dash - ${id ?? ''}`}</title>
             </Head>
 
             <h1 className={styles.name}>{station.name}</h1>
+
+            <WindCompass directionDegrees={data?.windDirectionDegrees} />
+
             <h2 className={styles.windSpeed}>
-                {data?.windSpeedKnots ?? "--"}
+                {data?.windSpeedKnots?.toFixed(2) ?? '--'}
                 <span>kt</span>
             </h2>
+
+            <h3 className={styles.gustSpeed}>
+                <span>Gust</span>
+                {data?.windGustKnots?.toFixed(2) ?? '--'}
+                <span>kt</span>
+            </h3>
         </div>
-    );
+    )
 }
 
 export async function getStaticPaths() {
-    const stations = await kitingApi.list();
+    const stations = await kitingApi.list()
 
     return {
         paths: stations.map(({ id }) => ({ params: { id: `${id}` } })),
-        fallback: "blocking",
-    };
+        fallback: 'blocking',
+    }
 }
 
 export async function getStaticProps({ params }) {
-    let station = await kitingApi.get(Number(params?.id));
+    let station = await kitingApi.get(Number(params?.id))
 
     if (!station) {
         return {
             notFound: true,
-        };
+        }
     }
 
     return {
         props: {
             station,
         },
-    };
+    }
 }
