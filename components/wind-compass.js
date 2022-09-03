9a697acc26bcc66d9b7d692d11826a52399221ac
Martin Dimitrov <martin@sachmata.com>
2022-09-03 17:42:41 +0300

    Test and fix wind code conversion

--- components/wind-compass.js
+++ components/wind-compass.js
@@ -1,30 +1,36 @@
 import clsx from 'clsx'
 import { useMemo } from 'react'
 
-const WIND_DIRECTION_CODE = [
-    'n',
-    'nne',
-    'ne',
-    'ene',
-    'e',
-    'ese',
-    'se',
-    'sse',
-    's',
-    'ssw',
-    'sw',
-    'wsw',
-    'w',
-    'wnw',
-    'nw',
-    'nnw',
-]
+// http://snowfence.umn.edu/Components/winddirectionanddegrees.htm
+export const WIND_DIRECTION_MAP = {
+    N: [348.75, 11.25],
+    NNE: [11.25, 33.75],
+    NE: [33.75, 56.25],
+    ENE: [56.25, 78.75],
+    E: [78.75, 101.25],
+    ESE: [101.25, 123.75],
+    SE: [123.75, 146.25],
+    SSE: [146.25, 168.75],
+    S: [168.75, 191.25],
+    SSW: [191.25, 213.75],
+    SW: [213.75, 236.25],
+    WSW: [236.25, 258.75],
+    W: [258.75, 281.25],
+    WNW: [281.25, 303.75],
+    NW: [303.75, 326.25],
+    NNW: [326.25, 348.75],
+}
 
+const WIND_DIRECTION_CODE = Object.keys(WIND_DIRECTION_MAP)
 const WIND_DIRECTION_SECTOR = 360 / WIND_DIRECTION_CODE.length
 
 export function windDirectionCode(degrees) {
-    const direction = ((degrees ?? 0) + WIND_DIRECTION_SECTOR / 2) % 360
-    const index = Math.round(direction / WIND_DIRECTION_SECTOR)
+    let _degrees = ((degrees ?? 0) + WIND_DIRECTION_SECTOR / 2) % 360
+    if (_degrees < 0) {
+        _degrees += 360
+    }
+
+    const index = Math.floor(_degrees / WIND_DIRECTION_SECTOR)
     const code = WIND_DIRECTION_CODE[index]
 
     return code
@@ -38,7 +44,7 @@ export default function WindCompass({ directionDegrees }) {
             <div className="direction">
                 <p>{windCode?.toUpperCase() ?? ''}</p>
             </div>
-            <div className={clsx('arrow', windCode)}></div>
+            <div className={clsx('arrow', windCode?.toLowerCase())}></div>
         </div>
     )
 }
--- components/wind-compass.test.js
+++ components/wind-compass.test.js
@@ -1,9 +1,37 @@
-import { windDirectionCode } from './wind-compass'
+import { windDirectionCode, WIND_DIRECTION_MAP } from './wind-compass'
 
 describe('WindDirectionCode', () => {
     it('returns North for empty', () => {
         const code = windDirectionCode()
 
-        expect(code).toBe('n')
+        expect(code).toBe('N')
     })
+
+    for (const windCode in WIND_DIRECTION_MAP) {
+        const [from, to] = WIND_DIRECTION_MAP[windCode]
+
+        it(`returns '${windCode}' for positive [${from} - ${to}}]`, () => {
+            const codeFrom = windDirectionCode(from)
+            const codeTo = windDirectionCode(to - 1e-3)
+
+            expect(codeFrom).toBe(windCode)
+            expect(codeTo).toBe(windCode)
+        })
+
+        it(`returns '${windCode}' for negative [${from - 360} - ${to - 360}}]`, () => {
+            const codeFrom = windDirectionCode(from - 360)
+            const codeTo = windDirectionCode(to - 360 - 1e-3)
+
+            expect(codeFrom).toBe(windCode)
+            expect(codeTo).toBe(windCode)
+        })
+
+        it(`returns '${windCode}' for overflow [${from + 360} - ${to + 360}}]`, () => {
+            const codeFrom = windDirectionCode(from + 360)
+            const codeTo = windDirectionCode(to + 360 - 1e-3)
+
+            expect(codeFrom).toBe(windCode)
+            expect(codeTo).toBe(windCode)
+        })
+    }
 })
