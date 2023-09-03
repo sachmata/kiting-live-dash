0b37a83479bf87772182574557a5ec04f10b2f8d
Martin Dimitrov <martin@sachmata.com>
2023-09-03 10:01:03 +0300

    Add Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import '../styles/globals.css'
 import '../styles/wind-compass.scss'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    )
 }
 
 export default MyApp
